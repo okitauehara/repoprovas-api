6bf4ab02e707b1aeaa56df0c1622d030841b64d7
okitauehara <marcosokita@live.com>
2021-12-16 22:45:20 -0300

    feat/getProfessors: base router implemented

---
+++ src/routers/professorsRouter.ts
@@ -0,0 +1,8 @@
+import { Router } from 'express';
+import * as professorController from '../controllers/professorController';
+
+const router = Router();
+
+router.get('/:subjectId', professorController.getProfessorsById);
+
+export default router;
