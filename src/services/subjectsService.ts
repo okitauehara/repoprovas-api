4b71c7d5db704257940d550ab1f889d3db806af2
okitauehara <marcosokita@live.com>
2021-12-16 22:19:32 -0300

    feat/getSubjects: base service implemented

--- src/services/subjectsService.ts
+++ src/services/subjectsService.ts
@@ -1,5 +1,9 @@
-async function get() {
+import { getRepository } from 'typeorm';
+import Subjects from '../entities/Subjects';
 
+async function get() {
+  const result = await getRepository(Subjects).find();
+  return result;
 }
 
 export {
