92fd38f6f2d15fd1a7129d6567f83e7fe49dcad7
okitauehara <marcosokita@live.com>
2021-12-16 22:10:37 -0300

    feat/getSubjects: base controller implemented

--- src/controllers/subjectsController.ts
+++ src/controllers/subjectsController.ts
@@ -1,5 +1,14 @@
-async function getSubjects() {
+import { Request, Response, NextFunction } from 'express';
+import { HttpStatusCode } from '../enums/httpStatusCode';
+import * as subjectsService from '../services/subjectsService';
 
+async function getSubjects(req: Request, res: Response, next: NextFunction) {
+  try {
+    const result = await subjectsService.get();
+    return res.status(HttpStatusCode.OK).send(result);
+  } catch (error) {
+    next(error);
+  }
 }
 
 export {
