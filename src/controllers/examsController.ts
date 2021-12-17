a92706e4f6ab666cc7016d0c5610c407889bda96
okitauehara <marcosokita@live.com>
2021-12-17 13:28:16 -0300

    feat/postExam: base router implemented

---
+++ src/controllers/examsController.ts
@@ -0,0 +1,7 @@
+async function postExam() {
+
+}
+
+export {
+  postExam,
+};
--- src/router.ts
+++ src/router.ts
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import categoriesRouter from './routers/categoriesRouter';
 import professorsRouter from './routers/professorsRouter';
 import subjectsRouter from './routers/subjectsRouter';
+import examRouter from './routers/examsRouter';
 
 const router = Router();
 
 router.use('/categories', categoriesRouter);
 router.use('/subjects', subjectsRouter);
 router.use('/professors', professorsRouter);
+router.use('/exams', examRouter);
 
 export default router;
